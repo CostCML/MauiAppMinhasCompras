5d1c5ee9e11d8110fbf8e338ff5b3fe61109e043
CostCML <costcml@gmail.com>
2026-03-23 10:34:17 -0300

    Valdação nos campos de descriçao e preço para não serem inseridos registros nulos

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,12 +4,40 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+
+        string _descricao;
+
+        decimal _preco;
+
         [PrimaryKey, AutoIncrement]
 
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao { get => _descricao;
+            set
+            {
+                if (value == null) {
+                    throw new Exception("Por favor, preencha a descrição");
+                
+                }
+                _descricao = value;
+            }
+        
+        }
         public int Quantidade { get; set; }
-        public decimal Preco {  get; set; }
+        public decimal Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("O preço deve ser maior que zero");
+                }
+
+                _preco = value;
+            }
+        }
+
         public decimal Total => Quantidade * Preco;
     }
 }
